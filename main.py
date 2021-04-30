b2ced5823474177c12dd6cda04fd96f5dad538dc
Yogalholic <feynmanownage@gmail.com>
2021-04-30 13:33:29 +0200

    Implement Return Bid function

--- main.py
+++ main.py
@@ -17,18 +17,22 @@ import config
 exchange_id = 'kucoin'
 exchange_class = getattr(ccxt, exchange_id)
 kucoin = exchange_class({
-        # 'set_sandbox_mode': True,
-        'apiKey': config.Key,
-        'secret': config.Secret,
-        'password': config.passphrase,
+        # 'apiKey': config.Key,
+        # 'secret': config.Secret,
+        # 'password': config.passphrase,
         'timeout': 30000,
         'enableRateLimit': True,
 })
 kucoin.set_sandbox_mode(True)
-balance = kucoin.fetch_balance()
-print(balance)
-# okcoin1 = ccxt.okcoinusd ({ 'id': 'okcoin1' })
-# id = 'btcchina'
+# balance = kucoin.fetch_balance()
+# print(balance)
+def return_bid(symbol):
+        kucoin.load_markets(True)
+        orderbook = kucoin.fetch_l2_order_book (symbol,20)
+        return orderbook['bids'][1][0]
+print(return_bid('BTC/USDT'))
+# print(ccxt.kucoin().public_get_ticker_pair ({ 'pair': 'btc_usdt' }))
+# Python# id = 'btcchina'
 # btcchina = eval ('ccxt.%s ()' % id)
 # coinbasepro = getattr (ccxt, 'coinbasepro') ()
 
