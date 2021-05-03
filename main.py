d776594c1f2b8e8becb8635835a2066590a884df
Yogalholic <feynmanownage@gmail.com>
2021-05-03 16:05:02 +0200

    End of project

--- main.py
+++ main.py
@@ -1,47 +1,42 @@
 import ccxt
-import config
-# bitfinex = ccxt.bitfinex({
-#     'apiKey': config.loginBitfinex,
-#     'secret': config.passBitfinex,
-#     'timeout': 30000,
-#     'enableRateLimit': True,
-# }) # default id
-# kucoin = ccxt.kucoin({
-#     'apiKey': config.Key,
-#     'secret': config.Secret,
-#     'password': config.passphrase,
-#     'timeout': 30000,
-#     'enableRateLimit': True,
-# }) # default
-# from variable id
-exchange_id = 'kucoin'
+import schedule
+import time
+from random import seed
+from random import randint
+seed(2)
+txMax = # choose a maximum size of the order, it should be an integer
+txMin = # choose a minimum size of the order, it should be an integer
+pauseMax = #choose the longest interval between two orders
+pauseMin = #choose the shortest interval between two orders
+exchange_id = '' #name of the exchange
 exchange_class = getattr(ccxt, exchange_id)
-kucoin = exchange_class({
-        # 'apiKey': config.Key,
-        # 'secret': config.Secret,
-        # 'password': config.passphrase,
+exchange = exchange_class({
+        'apiKey': 'Key',
+        'secret': 'Secret',
+        # 'password': 'passphrase',#some exchanges ask for a passphrase
         'timeout': 30000,
         'enableRateLimit': True,
 })
-kucoin.set_sandbox_mode(True)
-# balance = kucoin.fetch_balance()
-# print(balance)
+# exchange.set_sandbox_mode(True) #connect to sandbox
 def return_bid(symbol):
-        kucoin.load_markets(True)
-        orderbook = kucoin.fetch_l2_order_book (symbol,20)
+        '''
+        :param symbol: a trading pair ('BTC-USDT',...)
+        :return: the second bid price in the orderbook
+        '''
+        exchange.load_markets(True)
+        orderbook = exchange.fetch_l2_order_book (symbol,20)
         return orderbook['bids'][1][0]
-print(return_bid('BTC/USDT'))
-# print(ccxt.kucoin().public_get_ticker_pair ({ 'pair': 'btc_usdt' }))
-# Python# id = 'btcchina'
-# btcchina = eval ('ccxt.%s ()' % id)
-# coinbasepro = getattr (ccxt, 'coinbasepro') ()
 
-# # from variable id
-# exchange_id = 'kucoin'
-# exchange_class = getattr(ccxt, exchange_id)
-# exchange = exchange_class({
-#     'apiKey': 'YOUR_API_KEY',
-#     'secret': 'YOUR_SECRET',
-#     'timeout': 30000,
-#     'enableRateLimit': True,
-# })
+symbol = 'BTC/USDT' #choose a trading pair
+
+def launchtrade(symbol):
+        bid = return_bid(symbol)
+        try:
+                order = exchange.create_limit_order(symbol, 'buy', randint(txMin, txMax), bid)
+                print(order)
+        except Exception as err:
+                print(err)
+schedule.every(randint(pauseMin, pauseMin)).minutes.do(launchtrade,symbol)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
